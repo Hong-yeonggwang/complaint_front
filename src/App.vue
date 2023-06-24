568f1cd673e4b56648c5577888db6d418c371f46
hong-yeong-gwang <chamsk0808@naver.com>
2023-06-24 18:54:36 +0900

    vuetify세팅 및 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <div class = "w-8 h-4 bg-black"></div>
+  <NavigationBar>
+  </NavigationBar>
+  <div class="w-4 h-4 bg-black"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NavigationBar,
+  },  
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
