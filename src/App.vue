c948b11f0572f71b32db88afa733a77919cf5346
hong-yeong-gwang <chamsk0808@naver.com>
2023-06-24 19:02:24 +0900

    네비게이션바 수정

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,14 @@
 <template>
-<<<<<<< HEAD
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer
-        v-model="drawer"
-        :rail="rail"
-        permanent
-        @click="rail = false"
-      >
-        <v-list-item
-          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
-          title="John Leider"
-          nav
-        >
-          <template v-slot:append>
-            <v-btn
-              variant="text"
-              icon="mdi-chevron-left"
-              @click.stop="rail = !rail"
-            ></v-btn>
-          </template>
-        </v-list-item>
-
-        <v-divider></v-divider>
-
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
-          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <v-main style="height: 250px"></v-main>
-    </v-layout>
-  </v-card>
-</template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-=======
-  <NavigationBar>
-  </NavigationBar>
-  <div class="w-4 h-4 bg-black"></div>
+  <NavigationBar></NavigationBar>
 </template>
 
 <script>
 import NavigationBar from './components/NavigationBar.vue'
->>>>>>> 568f1cd673e4b56648c5577888db6d418c371f46
 
 export default {
-  // name: 'App',
+  name: 'App',
   components: {
-<<<<<<< HEAD
-    // HelloWorld
+    NavigationBar
   },
 
   data () {
@@ -73,9 +30,3 @@ export default {
   margin-top: 60px;
 } */
 </style>
-=======
-    NavigationBar,
-  },  
-}
-</script>
->>>>>>> 568f1cd673e4b56648c5577888db6d418c371f46
