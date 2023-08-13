8e134be0c81dc989b5ca6b9590c6a7f78487eb76
DH Jeong <wjdehgns97@gmail.com>
2023-08-13 18:26:49 +0900

    구매 디자인 1차

--- src/components/Buy.vue
+++ src/components/Buy.vue
@@ -16,39 +16,98 @@
     </v-card-text>
   </v-card>
 
-
-  <!-- <form @submit.prevent="submit" action=""> -->
-  <form v-on:submit="submitForm" action="">
-    <v-card class="mx-auto max-w-500">
-      <div  class="mx-auto w-fit">
-        <!-- 버스 -->
-        <div v-if="this.tab == 'bus'">
-          <div v-for="(item, index) in bus" :key="index">
-            <div class="my-1">
-              {{ item.title }} {{ item.value }}
-              <v-btn density="compact" icon="mdi-minus" @click="minusBus(index)"></v-btn>
-              <input type="number" class="border-solid border-2 border-black mx-3" v-model="item.count">
-              <v-btn density="compact" icon="mdi-plus" @click="plusBus(index)"></v-btn>
-            </div>
+  <form v-on:submit="submitForm" action="" class="w-full">
+    <div class="py-6 w-56 m-auto">
+      <!-- 버스 -->
+      <div v-if="this.tab == 'bus'">
+        <div>
+          <div class="text-center float-left w-1/2">
+            출발지
+          </div>
+          <div class="text-center float-left w-1/2">
+            가격
           </div>
         </div>
-        <!-- 식당 -->
-        <div v-if="this.tab == 'meal'">
-          <div v-for="(item, index) in meal" :key="index">
-            <div cols="auto">
-              {{ item.title }} {{ item.value }}
-              <v-btn density="compact" icon="mdi-minus" @click="minusMeal(index)"></v-btn>
-              <input type="number" class="border-solid border-2 border-black" v-model="item.count">
-              <v-btn density="compact" icon="mdi-plus" @click="plusMeal(index)"></v-btn>
+        <div v-for="(item, index) in bus" :key="index">
+          <div class="mt-4">
+            <div>
+              <div class="text-center float-left w-1/2">
+                {{ item.title }}
+              </div>
+              <div class="text-center float-left w-1/2">
+                {{ item.value }}
+              </div>
+            </div>
+            <div>
+              <div class="float-left w-fit">
+                <v-btn density="compact" icon="mdi-minus" @click="minusBus(index)"></v-btn>
+              </div>
+              <div class="float-left">
+                <input type="number" class="border-solid border-2 border-black mx-3 w-36" v-model="item.count">
+              </div>
+              <div class="float-left w-fit">
+                <v-btn density="compact" icon="mdi-plus" @click="plusBus(index)"></v-btn>
+              </div>
+              <div class="clear-both"></div>
             </div>
           </div>
         </div>
       </div>
-    </v-card>
+      <!-- 식당 -->
+      <div v-if="this.tab == 'meal'">
+        <div>
+          <div class="text-center float-left w-1/2">
+            식당
+          </div>
+          <div class="text-center float-left w-1/2">
+            가격
+          </div>
+        </div>
+        <div v-for="(item, index) in meal" :key="index">
+          <div class="mt-4">
+            <div>
+              <div class="text-center float-left w-1/2">
+                {{ item.title }}
+              </div>
+              <div class="text-center float-left w-1/2">
+                {{ item.value }}
+              </div>
+            </div>
+            <div>
+              <div class="float-left w-fit">
+                <v-btn density="compact" icon="mdi-minus" @click="minusBus(index)"></v-btn>
+              </div>
+              <div class="float-left">
+                <input type="number" class="border-solid border-2 border-black mx-3 w-36" v-model="item.count">
+              </div>
+              <div class="float-left w-fit">
+                <v-btn density="compact" icon="mdi-plus" @click="plusBus(index)"></v-btn>
+              </div>
+              <div class="clear-both"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-6">
+        <div class="float-left">
+          <div>총 상품 금액</div>
+        </div>
+        <div class="float-left">
+          <div class="float-left">총 수량</div>
+          <div class="float-left"></div>
+          <div class="float-left">장</div>
+        </div>
+        <div class="float-right">
+          <div class="float-left"></div>
+          <div class="float-left">원</div>
+        </div>
+        <div class="clear-both"></div>
+      </div>
 
-    <v-btn class="me-4" type="submit">
-      submit
-    </v-btn>
+      <v-btn class="me-4 mt-4" type="submit">
+        구매하기
+      </v-btn>
+    </div>
   </form>
 </template>
 
@@ -83,17 +142,32 @@ export default {
       ],
       meal: [
         {
-          title: 'meal #1',
+          title: '인성관 #A',
           value: 4000,
           count: 0
         },
         {
-          title: 'meal #2',
+          title: '인성관 #B',
+          value: 4000,
+          count: 0
+        },
+        {
+          title: '환과대 #A',
           value: 4500,
           count: 0
         },
         {
-          title: 'meal #3',
+          title: '환과대 #B',
+          value: 4500,
+          count: 0
+        },
+        {
+          title: '생활관 #A',
+          value: 3500,
+          count: 0
+        },
+        {
+          title: '생활관 #B',
           value: 3500,
           count: 0
         },
